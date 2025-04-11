0c0a6d020982967029dff7e749ab5a6210f5e216
kcansino <cansino.kristian@gmail.com>
2025-04-11 18:01:57 +0800

    upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    
+    AOS.init();
+
+    lightbox.option({
+        'showImageNumberLabel':false
+    })
+
+    $('.works-owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        autoplay:false,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1000:{
+                items:2
+            },
+            1200:{
+                items:2
+            },
+            1300:{
+                items:3
+            }
+        }
+    })
+
+});
